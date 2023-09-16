54674e88d924af34627621aed5970c1061ad40e1
novakvova <novakvova@gmail>
2023-09-16 17:57:15 +0300

    Begin workign app

--- AppConsole/src/main/java/org/example/controllers/CategoryController.java
+++ AppConsole/src/main/java/org/example/controllers/CategoryController.java
@@ -1,15 +1,36 @@
 package org.example.controllers;
 
+import lombok.AllArgsConstructor;
+import org.example.dto.category.CategoryCreateDTO;
+import org.example.entities.CategoryEntity;
+import org.example.repositories.CategoryRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class CategoryController {
 
+    private final CategoryRepository categoryRepository;
     @GetMapping("/")
     public ResponseEntity<String> index() {
         return new ResponseEntity<>("Привіт", HttpStatus.OK);
+    }
+
+    @PostMapping("/create")
+    public CategoryEntity create(@RequestBody CategoryCreateDTO dto)
+    {
+        var cat = CategoryEntity
+                .builder()
+                .name(dto.getName())
+                .description(dto.getDescription())
+                .image(dto.getImage())
+                .build();
+        categoryRepository.save(cat);
+        return cat;
     }
 }
