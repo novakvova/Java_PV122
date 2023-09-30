791731c6a5f0db73d5265c1a9dc685556eb93f53
novakvova <novakvova@gmail>
2023-09-30 16:01:41 +0300

    Begin workign auth userdetails service

--- AppConsole/src/main/java/org/example/Main.java
+++ AppConsole/src/main/java/org/example/Main.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import org.example.services.SeedService;
 import org.example.storage.StorageProperties;
 import org.example.storage.StorageService;
 import org.springframework.boot.CommandLineRunner;
@@ -19,10 +20,12 @@ public class Main {
     }
 
     @Bean
-    CommandLineRunner init(StorageService storageService) {
+    CommandLineRunner init(StorageService storageService, SeedService seedService) {
         return (args)-> {
             try {
                 storageService.init();
+                seedService.seedRoleData();
+                seedService.seedUserData();
             }catch(Exception ex) {
                 System.out.println("--------Хюстон у нас проблеми--------"+ex.getMessage());
             }
