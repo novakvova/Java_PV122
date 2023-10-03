8a24892aa98cfd6c943047ea91bb39411987cb34
novakvova <novakvova@gmail>
2023-10-03 20:04:02 +0300

    Add login form

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -3,7 +3,7 @@ import { Suspense, lazy, useEffect, useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 
-import ECommerce from './pages/Dashboard/ECommerce';
+// import ECommerce from './pages/Dashboard/ECommerce';
 import SignIn from './pages/Authentication/SignIn';
 import SignUp from './pages/Authentication/SignUp';
 import Loader from './common/Loader';
@@ -27,10 +27,15 @@ function App() {
           <Toaster position='top-right' reverseOrder={false} containerClassName='overflow-auto'/>
 
           <Routes>
+              <Route path="/" element={
+                  <Suspense fallback={<Loader/>}>
+                      <SignIn/>
+                  </Suspense>
+              }/>
               <Route path="/auth/signin" element={<SignIn />} />
               <Route path="/auth/signup" element={<SignUp />} />
               <Route element={<DefaultLayout />}>
-                  <Route index element={<ECommerce />} />
+                  {/*<Route index element={<ECommerce />} />*/}
                   {routes.map(({ path, component: Component }) => (
                       <Route
                           path={path}
--- my-react-app/src/pages/Authentication/SignIn.tsx
+++ my-react-app/src/pages/Authentication/SignIn.tsx
@@ -146,20 +146,21 @@ const SignIn = () => {
 
           <div className="w-full border-stroke dark:border-strokedark xl:w-1/2 xl:border-l-2">
             <div className="w-full p-4 sm:p-12.5 xl:p-17.5">
-              <span className="mb-1.5 block font-medium">Start for free</span>
+              {/*<span className="mb-1.5 block font-medium">Start for free</span>*/}
               <h2 className="mb-9 text-2xl font-bold text-black dark:text-white sm:text-title-xl2">
-                Sign In to TailAdmin
+                Вхід на сайт
               </h2>
 
               <form>
+
                 <div className="mb-4">
                   <label className="mb-2.5 block font-medium text-black dark:text-white">
-                    Email
+                    Електронна пошта
                   </label>
                   <div className="relative">
                     <input
                       type="email"
-                      placeholder="Enter your email"
+                      placeholder="Вкажіть пошту"
                       className="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
                     />
 
@@ -183,14 +184,14 @@ const SignIn = () => {
                   </div>
                 </div>
 
-                <div className="mb-6">
+                <div className="mb-4">
                   <label className="mb-2.5 block font-medium text-black dark:text-white">
-                    Re-type Password
+                    Пароль
                   </label>
                   <div className="relative">
                     <input
                       type="password"
-                      placeholder="6+ Characters, 1 Capital letter"
+                      placeholder="6+ Символів, 1 Велика літера"
                       className="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
                     />
 
@@ -221,7 +222,7 @@ const SignIn = () => {
                 <div className="mb-5">
                   <input
                     type="submit"
-                    value="Sign In"
+                    value="Вхід"
                     className="w-full cursor-pointer rounded-lg border border-primary bg-primary p-4 text-white transition hover:bg-opacity-90"
                   />
                 </div>
@@ -260,14 +261,14 @@ const SignIn = () => {
                       </defs>
                     </svg>
                   </span>
-                  Sign in with Google
+                  Вхід через Google
                 </button>
 
                 <div className="mt-6 text-center">
                   <p>
-                    Don’t have any account?{' '}
+                    У вас немає облікового запису?{' '}
                     <Link to="/auth/signup" className="text-primary">
-                      Sign Up
+                      Реєстрація
                     </Link>
                   </p>
                 </div>
