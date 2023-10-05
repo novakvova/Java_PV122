fbd30541d19e1df0f9e122d80592463ef963b165
novakvova <novakvova@gmail>
2023-10-05 17:47:58 +0300

    Begin working Login

--- my-react-app/src/pages/Authentication/SignIn/index.tsx
+++ my-react-app/src/pages/Authentication/SignIn/index.tsx
@@ -1,11 +1,41 @@
 import { Link } from 'react-router-dom';
-import LogoDark from '../../images/logo/logo-dark.svg';
-import Logo from '../../images/logo/logo.svg';
-import InputGroup from "../../common/InputGroup";
+import LogoDark from '../../../images/logo/logo-dark.svg';
+import Logo from '../../../images/logo/logo.svg';
+import InputGroup from "../../../common/InputGroup";
+import {ILogin, ITokenResponse} from "./types.ts";
+import * as Yup from "yup";
+import {useFormik} from "formik";
+import http_common from "../../../http_common.ts";
 
 
 const SignIn = () => {
-  //const LogoDark = "http://localhost:8085/uploading/150_a81ea56d-3c68-486d-8b42-5b40697ab11b.webp";
+
+  const initValue: ILogin = {
+    email: "",
+    password: ""
+  }
+
+  const validator = Yup.object().shape({
+    email: Yup.string().required("Вкажіть пошту").email("Не віркно вказано пошту"),
+    password: Yup.string().required("Вкажіть пароль")
+  });
+
+  const onHandlerSubmit = async (values: ILogin) => {
+    try {
+      const result = await http_common.post<ITokenResponse>("/api/account/login", values);
+      console.log("Result server login", result.data);
+    }catch(ex) {
+      console.log("Error", ex);
+    }
+  }
+
+  const formik= useFormik({
+    initialValues: initValue,
+    onSubmit: onHandlerSubmit,
+    validationSchema: validator
+  });
+
+  const {touched, handleChange, handleBlur, errors, values, handleSubmit} = formik;
   return (
     <>
       <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
@@ -154,40 +184,29 @@ const SignIn = () => {
                 Вхід на сайт
               </h2>
 
-              <form>
+              <form onSubmit={handleSubmit}>
 
-                <div className="mb-4">
-                  <label className="mb-2.5 block font-medium text-black dark:text-white">
-                    Електронна пошта
-                  </label>
-                  <div className="relative">
-                    <input
-                      type="email"
-                      placeholder="Вкажіть пошту"
-                      className="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
-                    />
+                <InputGroup
+                    field={"email"}
+                    label={"Електронна пошта"}
+                    type={"email"}
+                    placeholder={"Вкажіть пошту"}
+                    value={values.email}
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    touched={touched.email}
+                    error={errors.email} />
 
-                    <span className="absolute right-4 top-4">
-                      <svg
-                        className="fill-current"
-                        width="22"
-                        height="22"
-                        viewBox="0 0 22 22"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <g opacity="0.5">
-                          <path
-                            d="M19.2516 3.30005H2.75156C1.58281 3.30005 0.585938 4.26255 0.585938 5.46567V16.6032C0.585938 17.7719 1.54844 18.7688 2.75156 18.7688H19.2516C20.4203 18.7688 21.4172 17.8063 21.4172 16.6032V5.4313C21.4172 4.26255 20.4203 3.30005 19.2516 3.30005ZM19.2516 4.84692C19.2859 4.84692 19.3203 4.84692 19.3547 4.84692L11.0016 10.2094L2.64844 4.84692C2.68281 4.84692 2.71719 4.84692 2.75156 4.84692H19.2516ZM19.2516 17.1532H2.75156C2.40781 17.1532 2.13281 16.8782 2.13281 16.5344V6.35942L10.1766 11.5157C10.4172 11.6875 10.6922 11.7563 10.9672 11.7563C11.2422 11.7563 11.5172 11.6875 11.7578 11.5157L19.8016 6.35942V16.5688C19.8703 16.9125 19.5953 17.1532 19.2516 17.1532Z"
-                            fill=""
-                          />
-                        </g>
-                      </svg>
-                    </span>
-                  </div>
-                </div>
-
-                <InputGroup label={"Пароль"} error={""} />
+                <InputGroup
+                    field={"password"}
+                    label={"Пароль"}
+                    type={"password"}
+                    placeholder={"Вкажіть пароль"}
+                    value={values.password}
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    touched={touched.password}
+                    error={errors.password} />
 
                 <div className="mb-5">
                   <input
